38a3e85e1e1dcd063179945144795ce3f6013f3b
farbod Aprin <aprin.farbod@gmail.com>
2018-07-14 15:01:12 +0200

    fix the css hover button

--- src/commponents/Cockpit/Cockpit.js
+++ src/commponents/Cockpit/Cockpit.js
@@ -4,20 +4,6 @@ import "./Cockpit.css"
 const cockpit = (props) => {
 
 
-    const Button_Style = {
-        position: 'auto',
-        backgroundColor: 'green',
-        color: 'white',
-        font: 'inherit',
-        border: '1px solid blue',
-        padding: '8px',
-        cursor: 'pointer',
-        ':hover': {
-            backgroundColor: 'lightgreen',
-            color: 'black'
-        }
-    };
-
     let classes = [];
     if (props.persons.length <= 2) {
         classes.push('red'); // classes = ['red']
@@ -27,15 +13,13 @@ const cockpit = (props) => {
     }
     return (
         <div className={"classes.Cockpit"}>
-            {/*<div className={"bg"}>*/}
                 <h3 className={classes.join(' ')}> My Udemy Exersice </h3>
-                <button style={Button_Style} onClick={props.clicked}>
+                <button className={"button button1"} onClick={props.clicked}>
                     Toggle Persons
                 </button>
                 <p className={classes.join(' ')}>Fusce ut placerat eros. Aliquam consequat in augue sed
                     convallis. Donec orci urna, tincidunt
                 </p>
-            {/*</div>*/}
         </div>
     );
 };
